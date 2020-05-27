5a850718deba52ed7d263eee489191fb4c9793bd
Rohan Maheshwari <rohanmah1998@gmail.com>
2020-05-28 01:17:00 +0530

    Added recursive bfs

--- graph/basics/recursive_bfs.cpp
+++ graph/basics/recursive_bfs.cpp
@@ -15,8 +15,39 @@ using namespace std;
 #define rall(a) a.rbegin(),a.rend()
 #define print(a) cout<<a<<"\n"
 const ll mod=1e9+7;
+vector<vector<ll>>adjList;
+vector<ll>visited;
+void BFS(queue<ll>&q){
+    if(q.empty())
+        return;
+    ll v=q.front();
+    q.pop();
+    cout<<v<<" ";
+    for(auto &u:adjList[v]){
+        if(!visited[u]){
+            visited[u]=true;
+            q.push(u);
+        }
+    }
+    BFS(q);
+}
 void solve(){
-    
+    ll n,m;
+    cin>>n>>m;
+    adjList.resize(n+1);
+    visited.resize(n+1,false);
+    For(i,0,m){
+        ll a,b;
+        cin>>a>>b;
+        adjList[a].pb(b);
+        adjList[b].pb(a);
+    }
+    queue<ll>q;
+    ll root;
+    cin>>root;
+    q.push(root);
+    visited[root]=true;
+    BFS(q);
 }
 int main(){
     ios::sync_with_stdio(false),cin.tie(NULL);
